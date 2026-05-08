61b057902f4877b6f6033f56de204df15150a6f8
arabeman <ambinintsoamikaia@gmail.com>
2026-05-08 07:12:59 +0300

    feat(act1): add act_1 map loading and dynamic resize

--- game/scenes/Act1Scene.js
+++ game/scenes/Act1Scene.js
@@ -1,14 +1,27 @@
-import { Scene } from "@tialops/maki";
+import { Scene, manager } from "@tialops/maki";
 
-export default class Act1Scene extends Scene {
+class Act1Scene extends Scene {
+  constructor() {
+    super({ key: "Act1Scene" });
+  }
+
+  init() {
+    this.scale.resize(640, 448);
+  }
+
   preload() {
     super.preload();
+    manager.map(this, "act_1");
+    manager.preload(this);
   }
 
   create() {
     super.create();
+    manager.create(this);
   }
 
   update(time) {
   }
 }
+
+export { Act1Scene };
