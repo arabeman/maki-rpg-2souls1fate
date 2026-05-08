914933b7bad4cda2f5d78b839598d23f54a94086
arabeman <ambinintsoamikaia@gmail.com>
2026-05-08 07:48:13 +0300

    feat(act1): add player spawn at x:16 y:128 with position logging

--- game/scenes/Act1Scene.js
+++ game/scenes/Act1Scene.js
@@ -1,4 +1,7 @@
 import { Scene, manager } from "@tialops/maki";
+import { PlayerController } from "../core/PlayerController.js";
+import { SpriteLoader } from "../core/SpriteLoader.js";
+import { Dialog } from "../components/Dialog.js";
 
 class Act1Scene extends Scene {
   constructor() {
@@ -11,6 +14,7 @@ class Act1Scene extends Scene {
 
   preload() {
     super.preload();
+    SpriteLoader.load(this, "player", "player");
     manager.map(this, "act_1");
     manager.preload(this);
   }
@@ -18,10 +22,23 @@ class Act1Scene extends Scene {
   create() {
     super.create();
     manager.create(this);
+
+    this.player = PlayerController.create(this, 16, 128, "player");
+    this.keys = PlayerController.setupInput(this);
+    SpriteLoader.createAnims(this, "player", "player");
+
+    this.physics.add.collider(this.player, manager.getWallGroup(this, "act_1"));
+
     this.cameras.main.fadeIn(500);
   }
 
   update(time) {
+    if (!Dialog.isOpen()) {
+      PlayerController.handleMovement(this.player, this.keys);
+      PlayerController.handleAnimation(this.player, this.keys, time);
+    }
+    console.log(`Player position: x=${this.player.x.toFixed(1)}, y=${this.player.y.toFixed(1)}`);
+    Dialog.update(time);
   }
 }
 
