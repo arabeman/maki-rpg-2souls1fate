2f9d37b1bf4511b951570df38e073417808ac992
arabeman <ambinintsoamikaia@gmail.com>
2026-05-10 22:58:46 +0300

    feat(act3): add transition to EndScene on right side

--- game/scenes/act3/Act3MainScene.js
+++ game/scenes/act3/Act3MainScene.js
@@ -213,6 +213,22 @@ class Act3Scene extends Scene {
       );
     }
 
+    if (!this.sceneTransitioning && this.player.y > ACT3_MAP_HEIGHT && this.player.x >= ACT3_MAP_WIDTH / 2) {
+      this.sceneTransitioning = true;
+      this.cameras.main.fadeOut(500);
+      this.cameras.main.once("camerafadeoutcomplete", () =>
+        this.scene.start("EndScene"),
+      );
+    }
+
+    if (!this.sceneTransitioning && this.player.y < 0 && this.player.x >= ACT3_MAP_WIDTH / 2) {
+      this.sceneTransitioning = true;
+      this.cameras.main.fadeOut(500);
+      this.cameras.main.once("camerafadeoutcomplete", () =>
+        this.scene.start("EndScene"),
+      );
+    }
+
     if (!this.sceneTransitioning && this.player.x < 0) {
       this.sceneTransitioning = true;
       GameState.enteredAct2FromAct3 = true;
