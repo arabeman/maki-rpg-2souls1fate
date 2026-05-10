8e1f7df27c1c93dd57bc130abc6dd97b479054de
arabeman <ambinintsoamikaia@gmail.com>
2026-05-10 06:34:01 +0300

    feat(act2): add equipment handling and viewport settings

--- game/scenes/act2/Act2MainScene.js
+++ game/scenes/act2/Act2MainScene.js
@@ -1,4 +1,7 @@
 import { Scene, manager } from "@tialops/maki";
+import { Equipment } from "../../core/Equipment.js";
+import { GameState } from "../../data/dialogs.js";
+import { Inventory } from "../../core/Inventory.js";
 import { PlayerController } from "../../core/PlayerController.js";
 import { SpriteLoader } from "../../core/SpriteLoader.js";
 
@@ -7,6 +10,8 @@ const ACT2_MAP_WIDTH_TILES = 45;
 const ACT2_MAP_HEIGHT_TILES = 34;
 const ACT2_MAP_WIDTH = ACT2_MAP_WIDTH_TILES * ACT2_TILE_SIZE;
 const ACT2_MAP_HEIGHT = ACT2_MAP_HEIGHT_TILES * ACT2_TILE_SIZE;
+const ACT2_VIEWPORT_WIDTH = 640;
+const ACT2_VIEWPORT_HEIGHT = 448;
 
 class Act2Scene extends Scene {
   constructor() {
@@ -14,7 +19,7 @@ class Act2Scene extends Scene {
   }
 
   init() {
-    this.scale.resize(ACT2_MAP_WIDTH, ACT2_MAP_HEIGHT);
+    this.scale.resize(ACT2_VIEWPORT_WIDTH, ACT2_VIEWPORT_HEIGHT);
     this.cameras.main.setZoom(1.4);
   }
 
@@ -38,6 +43,12 @@ class Act2Scene extends Scene {
     this.keys = PlayerController.setupInput(this);
     SpriteLoader.createAnims(this, "player", "player");
     this.physics.add.collider(this.player.hitbox, manager.getWallGroup(this, "act_2"));
+    if (GameState.hasWeapon) {
+      const weaponItem = Inventory.getLastBySlot("mainHand");
+      if (weaponItem) {
+        Equipment.equip(this, this.player, weaponItem);
+      }
+    }
 
     this.physics.world.setBounds(0, 0, ACT2_MAP_WIDTH, ACT2_MAP_HEIGHT);
     this.cameras.main.setBounds(0, 0, ACT2_MAP_WIDTH, ACT2_MAP_HEIGHT);
@@ -60,6 +71,7 @@ class Act2Scene extends Scene {
   update(time) {
     PlayerController.handleMovement(this.player, this.keys);
     PlayerController.handleAnimation(this.player, this.keys, time);
+    Equipment.update(this, this.player);
   }
 }
 
