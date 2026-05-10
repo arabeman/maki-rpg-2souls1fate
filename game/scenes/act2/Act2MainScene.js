b1a96f7acff7fcb91c85cdebb3546723a0e0f620
arabeman <ambinintsoamikaia@gmail.com>
2026-05-10 06:19:55 +0300

    feat(scene): add player setup and movement to Act2

--- game/scenes/act2/Act2MainScene.js
+++ game/scenes/act2/Act2MainScene.js
@@ -1,4 +1,12 @@
 import { Scene, manager } from "@tialops/maki";
+import { PlayerController } from "../../core/PlayerController.js";
+import { SpriteLoader } from "../../core/SpriteLoader.js";
+
+const ACT2_TILE_SIZE = 16;
+const ACT2_MAP_WIDTH_TILES = 45;
+const ACT2_MAP_HEIGHT_TILES = 34;
+const ACT2_MAP_WIDTH = ACT2_MAP_WIDTH_TILES * ACT2_TILE_SIZE;
+const ACT2_MAP_HEIGHT = ACT2_MAP_HEIGHT_TILES * ACT2_TILE_SIZE;
 
 class Act2Scene extends Scene {
   constructor() {
@@ -6,12 +14,13 @@ class Act2Scene extends Scene {
   }
 
   init() {
-    this.scale.resize(640, 448);
+    this.scale.resize(ACT2_MAP_WIDTH, ACT2_MAP_HEIGHT);
     this.cameras.main.setZoom(1.4);
   }
 
   preload() {
     super.preload();
+    SpriteLoader.load(this, "player", "player");
     manager.map(this, "act_2");
     manager.preload(this);
   }
@@ -20,9 +29,37 @@ class Act2Scene extends Scene {
     super.create();
     manager.create(this);
 
-    this.physics.world.setBounds(0, 0, 640, 448);
-    this.cameras.main.setBounds(0, 0, 640, 448);
+    this.player = PlayerController.create(
+      this,
+      8,
+      440,
+      "player",
+    );
+    this.keys = PlayerController.setupInput(this);
+    SpriteLoader.createAnims(this, "player", "player");
+    this.physics.add.collider(this.player.hitbox, manager.getWallGroup(this, "act_2"));
+
+    this.physics.world.setBounds(0, 0, ACT2_MAP_WIDTH, ACT2_MAP_HEIGHT);
+    this.cameras.main.setBounds(0, 0, ACT2_MAP_WIDTH, ACT2_MAP_HEIGHT);
+    this.cameras.main.startFollow(this.player, true, 0.03, 0.03);
     this.cameras.main.fadeIn(500);
+
+    this.time.addEvent({
+      delay: 1000,
+      loop: true,
+      callback: () => {
+        console.log(
+          "Player pos:",
+          Math.round(this.player.x),
+          Math.round(this.player.y),
+        );
+      },
+    });
+  }
+
+  update(time) {
+    PlayerController.handleMovement(this.player, this.keys);
+    PlayerController.handleAnimation(this.player, this.keys, time);
   }
 }
 
